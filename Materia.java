5a43582628f48ac2af576117329e43e98424cf30
Fooliano <126425462+Fooliano@users.noreply.github.com>
2023-02-26 20:11:00 -0700

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -8,16 +8,45 @@ public class Escuela {
     public Escuela(String nombre){
         this.Nombre=nombre;
     }
+    public void addAlumnos(){
+
+    }
+    public boolean elimAlumno(){
+
+    }
     public void ListadoAlumnos(){
 
     }
     public void listadoCarr (){
 
     }
+    public void listadoClase(){
+        
+    }
+    public void listadoClase(valor){
+        
+    }
     public void listadoClaseProm(){
-
+        for (int i = 0; i < Alumnos.length; i++) {
+            System.out.println(Clases[i]+" Promedio: "+Clases[i]/Clases.length);
+        }
     }
     public void listadoMatporCarr(){
+        for (int i = 0; i < Mat.length; i++) 
+            System.out.println(Mat[i]);
+        
+    }
+    public void AgregarMat(Carrera){
 
+        for (int i = 0; i < Alumnos.length; i++) {
+            if((Mat[i]==x))
+            Mat[i]=Carrera;
+        } 
+    }
+    public void ElimMat(Mat, Carrera){
+        for (int i = 0; i < Alumnos.length; i++) {
+            if((Mat[i]==x))
+            Mat[i]=null;
+        }
     }
 }
